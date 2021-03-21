c025b910446ae9345200f21a35d4bf38a6d8e60e
IMGSaibh <murmelauge87@gmx.de>
2021-03-22 00:20:28 +0100

    added jump

---
+++ Assets/Scripts/GeneratedInputAction.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @DeviceInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @DeviceInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""188726a9-6ad9-4f05-b8fe-30e839aafe72"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""4578f94d-d3d9-475d-9b28-c54c79bdec13"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""ca03596d-314d-4bdc-a394-2d1dcbbb68ea"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""f1a25a80-d264-487c-bc82-a9125dc077b3"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0237d5ec-03cc-4762-bc28-cfa81aed27d1"",
+                    ""path"": ""<Gamepad>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""01745f9c-1c86-4a93-b87a-5b11010e9d7b"",
+                    ""path"": ""<Gamepad>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""61e52656-740f-4215-83cf-0a02dc1cfd25"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""c9812775-e100-4c4e-9aba-145826908df6"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4c0a8bda-8cf3-46c2-af41-71e38cdc0d21"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
+    public struct PlayerActions
+    {
+        private @DeviceInput m_Wrapper;
+        public PlayerActions(@DeviceInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/MovingSphereRigidbody.cs
+++ Assets/Scripts/MovingSphereRigidbody.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MovingSphere : MonoBehaviour
+public class MovingSphereRigidbody : MonoBehaviour
 {
     private DeviceInput deviceInput;
+    private bool triggerJump = false;
 
-    private Vector3 velocity;
+
     private Vector2 gamepadInputStick;
+    private Vector3 velocity;
+    private Vector3 inputVelocity;
     private Vector3 projectOnPlane;
     private Quaternion rotationToCamForward;
 
-
     [SerializeField, Range(0f, 100f)] public float maxSpeed = 10f;
     [SerializeField, Range(0f, 300f)] public float maxAcceleration = 10f;
+    [SerializeField, Range(0.0f, 2f)] public float jumpHeight = 2f;
+    [SerializeField, Range(0f, 5f)] public float maxAirJumps = 2f;
+
+    private Rigidbody rBody;
+    public Collider col;
+    public LayerMask goundLayers;
+
 
 
     private void Awake()
     {
         this.deviceInput = new DeviceInput();
+        this.rBody = GetComponent<Rigidbody>();
+        this.col.GetComponent<Collider>();
+
+
     }
 
     private void OnEnable()
@@ -25,6 +38,9 @@ public class MovingSphere : MonoBehaviour
         this.deviceInput.Enable();
         this.deviceInput.Player.Move.performed += this.OnMove;
         this.deviceInput.Player.Move.canceled += this.OnMove;
+
+        this.deviceInput.Player.Jump.performed += this.OnJump;
+        this.deviceInput.Player.Jump.canceled += this.OnJump;
 
 
     }
@@ -40,9 +56,15 @@ public class MovingSphere : MonoBehaviour
 
     }
 
+    private void OnJump(InputAction.CallbackContext context) 
+    {
+        Debug.Log("OnJump");
+        this.triggerJump = context.ReadValueAsButton();
+    }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -53,27 +75,50 @@ public class MovingSphere : MonoBehaviour
 
     private void FixedUpdate()
     {
+        this.Jump();
         this.Move();
     }
 
     public void Move()
     {
+        // get velocity first of rigidbody
+        velocity = rBody.velocity;
+
         // displacement so movement will not be teleportation anymore e.g Controlling Velocity
         // see <see href="https://catlikecoding.com/unity/tutorials/movement/sliding-a-sphere/">HERE</see>
-        Vector3 inputVelocity = new Vector3(this.gamepadInputStick.x, 0f, this.gamepadInputStick.y) * this.maxSpeed;
+        inputVelocity = new Vector3(this.gamepadInputStick.x, 0f, this.gamepadInputStick.y) * this.maxSpeed;
 
         float maxSpeedChange = this.maxAcceleration * Time.deltaTime;
         this.velocity.x = Mathf.MoveTowards(this.velocity.x, inputVelocity.x, maxSpeedChange);
         this.velocity.z = Mathf.MoveTowards(this.velocity.z, inputVelocity.z, maxSpeedChange);
-        Vector3 movementSteps = this.velocity * Time.deltaTime;
+        //Vector3 movementSteps = this.velocity * Time.deltaTime;
 
         // camera indepandent movement
         this.projectOnPlane = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
         this.rotationToCamForward = Quaternion.LookRotation(this.projectOnPlane);
-        movementSteps = rotationToCamForward * movementSteps;
 
 
-        this.transform.localPosition += movementSteps;
+        //movementSteps = rotationToCamForward * movementSteps;
+        //this.transform.localPosition += movementSteps;
+
+        rBody.velocity = velocity;
+    }
+
+    public void Jump()
+    {
+        if (triggerJump && isGrounded())
+        {
+            // jumping is all about overcome gravity
+            velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            rBody.velocity = velocity;
+
+
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.max, col.bounds.min, col.bounds.extents.x, goundLayers);
     }
 
     private void OnDisable()
